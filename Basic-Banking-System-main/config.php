65351f343ccc61aebb6d24f23ab92062653c87e0
rutikwagh <92197426+rutikwagh@users.noreply.github.com>
2022-01-17 20:33:44 +0530

    Add files via upload

---
+++ Basic-Banking-System-main/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	$servername = "localhost:8111";
+        $username = "root";
+        $password = "";
+        $database = "sparksbank";
+
+// Create connection
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+	if(!$conn){
+		die("Unable to connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
